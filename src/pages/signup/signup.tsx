73f7de6e0363a70f00a5d2643614d80272e7cd6f
Luke Prosser <lukeprosser@Lukes-MBP.lan>
2023-04-18 05:53:20 +0100

    apply text shadow

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -5,10 +5,10 @@ export default function SignupPage() {
     <div className="h-screen bg-center bg-no-repeat bg-cover bg-intro bg-primary-red">
       <section className="flex flex-col items-center justify-center h-full p-8">
         <div className="text-center text-white">
-          <h2 className="mb-6 text-3xl font-bold leading-10">
+          <h2 className="mb-6 text-3xl font-bold leading-10 text-shadow-sm">
             Learn to code by watching others
           </h2>
-          <p className="font-medium leading-7">
+          <p className="font-medium leading-7 text-shadow-sm">
             See how experienced developers solve problems in real-time. Watching
             scripted tutorials is great, but understanding how developers think
             is invaluable.
@@ -16,7 +16,7 @@ export default function SignupPage() {
         </div>
         <div className="mt-16">
           <Banner>
-            <p>
+            <p className="text-shadow-sm">
               <span className="font-bold">Try it free for 7 days</span> then
               $20/mo. thereafter
             </p>
